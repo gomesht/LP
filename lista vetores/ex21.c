a1b32008be801b640a77eba6382959901253f746
Henrique Tavares <58818069+gomesht@users.noreply.github.com>
2025-05-31 12:09:06 -0300

    Add files via upload

--- lista vetores/ex21.c
+++ lista vetores/ex21.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
 int main(){
-    
+    int A[10], B[10], C[10];
+    for (int i = 0; i < 10; i++) {
+        printf("Digite o valor do elemento %d do vetor A: ", i + 1);
+        scanf("%d", &A[i]);
+    }
+    for (int i = 0; i < 10; i++) {
+        printf("Digite o valor do elemento %d do vetor B: ", i + 1);
+        scanf("%d", &B[i]);
+    }
+    for (int i =0; i <10; i++) {
+        C[i] = A[i] - B[i];
+    }
+   printf("Vetor C (A - B):[");
+    for (int i = 0; i < 10; i++) {
+        printf("%d, ", C[i]);
+    }
+    printf("]\n");
 }
